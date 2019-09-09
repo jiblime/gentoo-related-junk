2a3306bcb6a285c75bc5dd88520dce6eb3b4a8a5
jiblime <47689567+jiblime@users.noreply.github.com>
2019-09-09 05:08:40 -0700

    Update ld.broke.sh

--- ld.broke.sh
+++ ld.broke.sh
@@ -6,25 +6,49 @@
 # that the speed of linking with LLD makes up for the time spent making per-package CFLAGS every 5min.
 # Default package input is the last package emerged (if it is not a @set) and can be deleted and replaced.
 
+edit_conf()
+{
+printf "\033[1A"
+cd /etc/portage/package.cflags
+
+local  VAR=( {a..z} ); local CONF=( $(ls) )
+
+for i in "${!CONF[@]}"; do    local "${VAR[i]}=${CONF[i]}"; done
+
+for i in "${!CONF[@]}"; do    echo  "$i -- ${CONF[i]}"; done
+
+echo -e "\nChoose the conf file to append to: \c"
+
+read CHOICE
+
+messy="${CONF[$CHOICE]}"
+}
+
+
+pkg_guess()
+{
 export LAST=$(awk 'f{print;f=0} /"favorites": \[/{f=1}' /var/cache/edb/mtimedb | tr -d '"' | tr -d [:blank:] | head -n1)
 
 if [[ $LAST == @* ]]; then
         unset LAST
 fi
+}
 
-read -p "Package that broke? " -i "${LAST}" -e GDI
+pkg_guess
 
-printf "Reason? "
+read -p "Package to filter: " -i "$LAST" -e GDI
+printf "\033[1A"
+echo -e "\033[KReason? \c"
 read DOC
 
+edit_conf
+echo -e "\n${GDI}\c" >> /etc/portage/package.cflags/${messy}
 
-echo -e "\n${GDI}\c" >> /etc/portage/package.cflags/clang-lld.conf
+sed -i '$s/$/\ \*FLAGS\-\=\"\-fuse\-linker\-plugin\ \-fuse\-ld\=gold\ \-fuse\-ld\=lld\"/' /etc/portage/package.cflags/${messy}
 
-sed -i '$s/$/\ \*FLAGS\-\=\"\-fuse\-linker\-plugin\ \-fuse\-ld\=lld\"/' /etc/portage/package.cflags/clang-lld.conf
-
-echo " # "${DOC}"" >> /etc/portage/package.cflags/clang-lld.conf
+echo " # "${DOC}"" >> /etc/portage/package.cflags/"${messy}"
 
 
-echo "${GDI}" >> /etc/portage/package.cflags/clang-lld.conf
+echo "${GDI}" >> /etc/portage/package.cflags/"${messy}"
 
-sed -i '$s/$/\ \*FLAGS\+\=\"\-fuse\-ld\=bfd\"/' /etc/portage/package.cflags/clang-lld.conf
+sed -i '$s/$/\ \*FLAGS\+\=\"\-fuse\-ld\=bfd\"/' /etc/portage/package.cflags/${messy}
